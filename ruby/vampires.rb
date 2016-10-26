05a6b10594167e56f7e162c5fd25b22a8bbf6fb5
NickHimmel <himmel.nick@gmail.com>
2016-10-26 16:09:04 +0200

    update

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,15 +2,15 @@
 # interview_name = gets.chomp
 puts "How old are you?"
 interview_age = gets.chomp.to_i
-# puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-# interview_garlic = gets.chomp
-# puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-# interview_health = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+interview_garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+interview_health = gets.chomp
 
 # puts "My am #{interview_name}, I am #{interview_age} old! #{interview_garlic} to garlic bread, #{interview_health} to health insurance" 
 
 case 
-when interview_age < 100
+when interview_age < 100 && (interview_garlic || interview_health)
 	puts "Probably not a vampire."
 else
 	puts "Vampire!"
