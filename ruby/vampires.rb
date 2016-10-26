b3af2699a1779f579a96cd37f3b22c2370b6533a
NickHimmel <himmel.nick@gmail.com>
2016-10-26 15:48:16 +0200

    add ruby to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+interview_name = gets.chomp
+puts "How old are you? What year were you born?"
+interview_age = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+interview_garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+interview_health = gets.chomp
+
+puts "My name is #{interview_name}, I am #{interview_age} old! #{interview_garlic} to garlic bread, #{interview_health} to health insurance" 
