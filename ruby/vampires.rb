d40fb11350d88e3f6187b81395b366c0240b1b6b
NickHimmel <himmel.nick@gmail.com>
2016-10-26 16:43:37 +0200

    add ruby to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,15 @@
-# puts "What is your name?"
-# interview_name = gets.chomp
+# puts "How many employees will be processed"
+# employees = gets.chomp
+
+
+puts "What is your name?"
+interview_name = gets.chomp.downcase!
 puts "How old are you?"
 interview_age = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-interview_garlic = gets.chomp
+interview_garlic = gets.chomp.downcase!
 puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-interview_health = gets.chomp
+interview_health = gets.chomp.downcase!
 
 # puts "My am #{interview_name}, I am #{interview_age} old! #{interview_garlic} to garlic bread, #{interview_health} to health insurance" 
 
@@ -15,6 +19,8 @@ elsif interview_age >= 100 && (interview_garlic != "yes" || interview_health != 
 	puts "Probably a vampire."
 elsif interview_age >= 100 && (interview_garlic != "yes" && interview_health != "yes")
 	puts "Almost certainly a vampire!"
+elsif interview_name == "drake cula" || interview_name == "tu fang"
+	puts "Definitely a vampire"
 else
 	puts "Results inconclusive!"
 end
