66507050997598058e9a1e4a47a35d5793bf7edc
NickHimmel <himmel.nick@gmail.com>
2016-10-26 16:06:28 +0200

    add ruby to vampire.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,13 @@
-puts "What is your name?"
-interview_name = gets.chomp
+# puts "What is your name?"
+# interview_name = gets.chomp
 puts "How old are you?"
-interview_age = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-interview_garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-interview_health = gets.chomp
+interview_age = gets.chomp.to_i
+# puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+# interview_garlic = gets.chomp
+# puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+# interview_health = gets.chomp
 
-puts "My am #{interview_name}, I am #{interview_age} old! #{interview_garlic} to garlic bread, #{interview_health} to health insurance" 
+# puts "My am #{interview_name}, I am #{interview_age} old! #{interview_garlic} to garlic bread, #{interview_health} to health insurance" 
 
 case 
 when interview_age < 100
