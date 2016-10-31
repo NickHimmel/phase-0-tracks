d494f239197e9e2beb107db922107d78d67823b0
NickHimmel <himmel.nick@gmail.com>
2016-10-31 17:28:13 +0100

    add ruby to robot_translator.rb

---
+++ ruby/robot_translator.rb
@@ -0,0 +1,20 @@
+# ROBOT TRANSLATOR
+
+# If a letter is capitalized and it's in the first half of the alphabeht, it becomes "bloop".
+# ABCDEFGHIJKLM => bloop
+
+# Otherwise, if a letter is capitalized or it's the letter "e", it becomes "buzz".
+# NOPQRSTUVWXYZ or e => buzz
+
+# If it's not a letter at all, it becomes "boing".
+
+# Otherwise, it becomes "beep".
+
+# "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbuzzbeepboing"
+
+def robot_translator (string_translate)
+
+	
+end
+
+robot_translator("Happy Halloween!")
