7f446e869dc7e2926c31c012dcc388fedaf4a9ee
NickHimmel <himmel.nick@gmail.com>
2016-10-31 20:12:46 +0100

    finished video)

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -12,13 +12,40 @@
 
 # "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbuzzbeepboing"
 
-def translate_phrase(phrase)
-	#loop through a phase
-	char_index = 0
-	while char_index < phrase.length
-		puts phrase[char_index]
-		char_index += 1
+# BUSINESS LOGIC
+
+def translate_char(char)
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	halfway = alphabet.length / 2
+	is_capitalized = (char.upcase == char)
+
+	if !alphabet.index(char.downcase)
+		"boing"
+	elsif is_capitalized && alphabet.index(char.downcase) < halfway
+		"bloop"
+	elsif is_capitalized || char == "e"
+		"buzz"
+	else
+		"beep"
 	end
 end
 
-translate_phrase("Happy Halloween!")
+def translate_phrase(phrase)
+	#loop through a phase
+	char_index = 0
+	translated_response = ""
+	while char_index < phrase.length
+		translated_response << translate_char(phrase[char_index])
+		char_index += 1
+	end
+	translated_response
+end
+
+# USER INTERFACE
+
+loop do
+	puts "Enter a phrase to translate (or type 'q' for quit):"
+	inputted_phrase = gets.chomp
+	break if inputted_phrase == "q"
+	puts translate_phrase(inputted_phrase)
+end 
