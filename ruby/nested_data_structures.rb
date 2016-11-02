223c34738340ce60cfdd289ed56cb23c1256abed
NickHimmel <himmel.nick@gmail.com>
2016-11-02 17:05:26 +0100

    finish nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,13 +1,19 @@
 met_museum = {
 	painting: {
 		artist: "John Singer Sargent",
-		title: "Madame X"
+		title: "Madame X",
 		info: ["1883–84", "Oil on canvas", "located in gallery 771"]
-	}
+	},
 	sculpture: {
-
-	}
-	photo: {
-		photographer: ""
+		artist: "Cornelia Parker",
+		title: "Transitional Object (PsychoBarn)",
+		info: ["2016", "Wood", "located on the roof"]
+	},
+	photography: {
+		artist: "André Kertész",
+		title: "Chez Mondrian",
+		info: ["1926", "Gelatin silver print", "not on view"]
 	}
 }
+
+p met_museum
