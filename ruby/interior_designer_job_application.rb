e5624c7917babb8e0a3a4a8077b4d746d0c8084e
NickHimmel <himmel.nick@gmail.com>
2016-11-02 11:29:27 +0100

    add ruby

---
+++ ruby/interior_designer_job_application.rb
@@ -0,0 +1,11 @@
+job_application = {
+	"name" : "J'onn J'onzz"
+	"address" : "Syrtis Major Mars"
+	"email" : "manhunter@green.com"
+	"phone" : "111 2222"
+	"fave shade of blue" : "cerulean"
+	"wallpaper preferences" : ["Paisley", "Abstract woodsy scenes (no squirrels)"]
+	"ombre" : "Practically medieval in its appalling irrelevance"
+}
+
+p job_application
