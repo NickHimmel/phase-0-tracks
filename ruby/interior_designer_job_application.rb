709199e8a291dd8127e42914b782b2e5bc91ec3a
NickHimmel <himmel.nick@gmail.com>
2016-11-02 11:35:16 +0100

    add ruby

--- ruby/interior_designer_job_application.rb
+++ ruby/interior_designer_job_application.rb
@@ -1,11 +1,21 @@
 job_application = {
-	"name" : "J'onn J'onzz"
-	"address" : "Syrtis Major Mars"
-	"email" : "manhunter@green.com"
-	"phone" : "111 2222"
-	"fave shade of blue" : "cerulean"
-	"wallpaper preferences" : ["Paisley", "Abstract woodsy scenes (no squirrels)"]
-	"ombre" : "Practically medieval in its appalling irrelevance"
+	"name" => "J'onn J'onzz",
+	"address" => "Syrtis Major Mars",
+	"email" => "manhunter@green.com",
+	"phone" => "111 2222",
+	"fave shade of blue" => "cerulean",
+	"wallpaper preferences" => ["Paisley", "Abstract woodsy scenes (no squirrels)"],
+	"ombre" => "Practically medieval in its appalling irrelevance",
 }
 
+p job_application
+p job_application["ombre"]
+job_application["ombre"] = "Fierce"
+p job_application["ombre"]
+job_application["hired"] = true
+
+email_address = "email"
+p email_address
+p job_application[email_address]
+job_application["name"] + job_application["address"]
 p job_application
