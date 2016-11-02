b5b72374bb17483195831fdbe94ff3871cbb9a7c
NickHimmel <himmel.nick@gmail.com>
2016-11-02 16:51:33 +0100

    begin nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,13 @@
+met_museum = {
+	painting: {
+		artist: "John Singer Sargent",
+		title: "Madame X"
+		info: ["1883–84", "Oil on canvas", "located in gallery 771"]
+	}
+	sculpture: {
+
+	}
+	photo: {
+		photographer: ""
+	}
+}
