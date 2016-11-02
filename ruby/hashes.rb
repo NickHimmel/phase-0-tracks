8df0830df7b30fe7fbac5baef93d34c4e7482a7f
NickHimmel <himmel.nick@gmail.com>
2016-11-02 11:47:10 +0100

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+
+# Prompt the designer/user for all of this information.
+# Convert any user input to the appropriate data type.
+# Print the hash back out to the screen when the designer has answered all of the questions.
+# Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+# Print the latest version of the hash, and exit the program.
+
+
+#Write a method that prompts the designer/user for all the information
+
+#Write a method that checks data types
+
+#update key method
+
+#declare a hash
+#pass that hash to the method to get information
+#print that hash
+#pass that hash to the method to check data type
+#pass that hash to the update key method
+#print the hash
