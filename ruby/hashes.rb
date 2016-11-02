2c9881501bb1eeaaf64a042ca991ee83c56141cf
NickHimmel <himmel.nick@gmail.com>
2016-11-02 15:07:20 +0100

    add a print method

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,6 +5,10 @@
 # Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
 # Print the latest version of the hash, and exit the program.
 
+#Write a method that checks data types
+def data_types(client_info)
+	p client_info
+end
 
 #Write a method that prompts the designer/user for all the information
 
@@ -24,34 +28,38 @@ def client_details(client_info)
 	client_info
 end
 
-#Write a method that checks data types
-def data_types(client_info)
-	p client_info
-end
-
 #update key method
 def add_field(client_info)
 	puts "Add a new field to your client information! Or type \"none\"" 
-	new_field = gets.chomp.to_sym
+	new_field = gets.chomp
 	if new_field == "none"
 		puts "Ok!"
 	else
 		puts "Enter info for #{new_field}"
 		field_info = gets.chomp
-		client_info[new_field] = field_info
+		client_info[new_field.to_sym] = field_info
 	end
 	client_info
 end
 
+# print out information 
+def print_client(client_info)
+	client_info.each do |key, value|
+		puts "#{key}: #{value}"
+	end
+end
 #declare a hash
 client_information = {}
 
 #pass that hash to the method to get information
 client_details(client_information)
 #print that hash
-#pass that hash to the method to check data type
-data_types(client_information)
+
 #pass that hash to the update key method
 add_field(client_information)
+
+#pass that hash to the method to check data type
+data_types(client_information)
+
 #print the hash
-puts client_information
+print_client(client_information)
