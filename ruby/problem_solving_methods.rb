4e359aba78bf4abf156d93115c5ea25259989531
NickHimmel <himmel.nick@gmail.com>
2016-11-03 18:07:12 +0100

    implemented realise2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -62,3 +62,17 @@ fib(6)
 fib(100)
 #[0,1,1,2,3,5]
 #218922995834555169026
+
+#2 Visualization helps, you can google for definitions and examples, read through other pseudocode
+#3 These can be fustrating and scary once you are trying to impliment it, but that is part of the process. You just to develop stragies to identify how you feel and when you need to take a break
+#4 create a pseudocode for bubble sort
+# sort an arry of integers  using sorting algorithm  return sorted version of array 
+# create a bubble sort method that will take an array of integers
+# the method will sort intergers integers of smallest to biggest
+# check first array index with the next array index and if the first index value is larger swap position
+# if the same don't swap but check second array index with third array index. 
+# loop array until it is in order from smallest to biggest
+#5 implement a sorting method
+sorted_arr = arr.sort
+p sorted_arr
+p sorted_arr.reverse 
