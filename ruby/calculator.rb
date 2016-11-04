6c35e68757538875033f28817c9dcea13dd7f8cb
NickHimmel <himmel.nick@gmail.com>
2016-11-04 17:39:50 +0100

    add user input and convert method

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -12,7 +12,20 @@ def calculator (integer_1, operator, integer_2)
 	end
 end
 
+def to_int(user_input)
+	new_string = p user_input.split
+	new_string[0] = new_string[0].to_i
+	new_string[2] = new_string[2].to_i
+	new_string
+end
+
 p calculator(4, '+', 5)
 p calculator(10, '/', 5)
 p calculator(25, '-', 5)
 p calculator(10, '*', 5)
+
+puts 'Please enter a calculation. num (+, /, -, or *) num2'
+user_input = gets.chomp
+user_input = to_int(user_input)
+p user_input
+p calculator(user_input[0], user_input[1], user_input[2])
