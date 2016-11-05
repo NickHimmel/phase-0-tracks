ab45313e42b81752239f4c3fe4198a32efde9ba9
NickHimmel <himmel.nick@gmail.com>
2016-11-05 10:51:07 +0100

    clean up driver code in order to create data structure for last realise

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,11 +13,8 @@
 
 #To Start write methods for the different parameters and make sure they work before writing code.
 
-#Method 1 swapping the first and last name
-def downcase_string (secret_name)
-	secret_name.downcase!
-end
-
+#need a method to capitalize both the first and last names of the new secret name.
+#need to join the single characters back into a string, then split it again, capitalize each and rejoin it
 def join_capitalize (secret_name)
 	secret_name = secret_name.join('')
 	secret_name = secret_name.split(' ')
@@ -26,6 +23,7 @@ def join_capitalize (secret_name)
 	secret_name = secret_name.join(' ')
 end
 
+#Method 1 swapping the first and last name
 def name_swap (secret_name)
 	secret_name = secret_name.split(' ')
 	secret_name = secret_name.reverse
@@ -38,16 +36,21 @@ end
 # (e, i, o, u, or a)
 def vowel_swap (secret_name)
 	#first separate string into an array of characters
+	#the loop through looking for vowels
+	#use each do loop to then go through each character
+	#check to see if it is a vowel using gsub and replace with the next vowel
+	#have to replace with an uppercase vowel, so when a becomes 'e' and then I check for 'e's, those 'a's don't become 'i's and etc
 	secret_name = secret_name.split('')
-	secret_name.map do |letters|
+	secret_name.each do |letters|
 		letters.gsub!(/[a]/, "E")
 		letters.gsub!(/[e]/, "I")
 		letters.gsub!(/[i]/, "O")
 		letters.gsub!(/[o]/, "U")
 		letters.gsub!(/[u]/, "A")
 	end
+	#then downcase eveything
 	secret_name.map!{|letter| letter.downcase}
-	#loop through looking for vowels
+	
 end
 
 #Method 3 Changing all of the consonants to next consonant
@@ -74,32 +77,35 @@ def consonant_swap (secret_name)
 	secret_name
 end
 
-def user_input
+#driver code
 
-	while true
-		puts 'Enter agents full name to get super cool secret agent name. Type \'quit\' to exit'
-		agent_name = gets.chomp
-		downcase_string(agent_name)
-		if agent_name == 'quit'
-			break
-		else
-			agent_name = name_swap(agent_name)
-			agent_name = vowel_swap(agent_name)
-			agent_name = consonant_swap(agent_name)
-			agent_name = join_capitalize(agent_name)
-			p agent_name
-		end
+#Use a data structure to store the fake names as they are entered. When the user exits the program, iterate through the data structure and print all of the data the user entered. A sentence like "Vussit Gimodoe is actually Felicia Torres" or "Felicia Torres is also known as Vussit Gimodoe" for each agent is fine.
+#Create two variables one for the original name and one for the converted name
+#loop to generate name
+loop do
+	puts 'Enter agents full name to get super cool secret agent name. Type \'quit\' to exit'
+	agent_name = gets.chomp
+	secret_name = agent_name.downcase
+	if secret_name == 'quit'
+		break
+	else
+		secret_name = name_swap(secret_name)
+		secret_name = vowel_swap(secret_name)
+		secret_name = consonant_swap(secret_name)
+		secret_name = join_capitalize(secret_name)
+		p secret_name
 	end
-
 end
 
-#Method 4 to join and capatlize 
-secret_agent = "Felicia Torres"
-downcase_string(secret_agent)
-secret_agent = name_swap(secret_agent)
-secret_agent = vowel_swap(secret_agent)
-secret_agent = consonant_swap(secret_agent)
-secret_agent = join_capitalize(secret_agent)
-user_input
-p secret_agent
+
+
+#first test they gave us
+# secret_agent = "Felicia Torres"
+# downcase_string(secret_agent)
+# secret_agent = name_swap(secret_agent)
+# secret_agent = vowel_swap(secret_agent)
+# secret_agent = consonant_swap(secret_agent)
+# secret_agent = join_capitalize(secret_agent)
+
+# p secret_agent
 # GOAL "Felicia Torres" will become "Vussit Gimodoe", 
