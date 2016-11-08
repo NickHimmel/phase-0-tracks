78b5ab00af907276321077151f24a9d57fd9d7e2
NickHimmel <himmel.nick@gmail.com>
2016-11-08 17:03:50 +0100

    add new_item list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -9,9 +9,15 @@
 # output: List will be a hash (items are keys, quantity is value)
 
 # Method to add an item to a list
+# example ("milk" 2)
 # input: item name and optional quantity
-# steps:
-# output:
+# steps: 
+# Create a method that is going to take three parameters, the master list, the item and the quantity.
+# Make the new item a new key with default value of 1 and add it to the master list
+# If there is a integer add it as the value of the new item
+# First check if there is integer 
+# If not integer default would be 1
+# output: finished list with added item and its quantity
 
 # Method to remove an item from the list
 # input:
@@ -28,16 +34,25 @@
 # steps:
 # output:
 
+list = {} 
 inputted_list = "bananas tomatoes apples bread"
 
-def add_items(inputted_list) 
-    list = {}
+
+def add_items(list, inputted_list) 
+    
     list_array = inputted_list.split(' ')
     
     list_array.each do |item|
-        list[item] = [1]
-        p list
+        list[item] = 1
     end
+    
 end
 
-add_items(inputted_list)
+def new_item(list, item, quantity = 1)
+	list[item] = quantity 
+	p list
+end
+
+add_items(list, inputted_list)
+new_item(list, "milk")
+
