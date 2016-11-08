a742c02bd0fa016b96a9ce551ac8f0441fcd2720
NickHimmel <himmel.nick@gmail.com>
2016-11-08 16:15:26 +0100

    update all

--- ruby/house_maker.rb
+++ ruby/house_maker.rb
@@ -26,6 +26,23 @@ def print_house(house)
 	p house
 end
 
+# Let the user add rooms
+can_add_rooms = true
+
+# Stoop loop when rooms cannot be added
+
+while can_add_rooms
+	# Get a room name from the user
+	puts "Type the name of a room to add (or type 'done'):"
+	room_name = gets.chomp
+	# If the user is done, stop loop
+	break if room_name == 'done'
+	# Otherwise, add the room to the house
+	can_add_rooms = add_room_to_house!(house, room_name)
+end
+
+
+
 # TEST CODE
 
 rooms = ['Living room', 'bedroom', 'bathroom', 'kitchen', 'bedroom 2', 'bedroom 3']
