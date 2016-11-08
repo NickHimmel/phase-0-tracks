12aecdae6765f42c397597020190aa8657a12a00
NickHimmel <himmel.nick@gmail.com>
2016-11-08 17:40:25 +0100

    refactor methods so far

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -13,6 +13,8 @@
 # input: item name and optional quantity
 # steps: 
 # Create a method that is going to take three parameters, the master list, the item and the quantity.
+# to search the hash to see if one of the keys is equal to item.
+# method to take your string and return true or fall 
 # Make the new item a new key with default value of 1 and add it to the master list
 # If there is a integer add it as the value of the new item
 # First check if there is integer 
@@ -34,25 +36,26 @@
 # steps:
 # output:
 
-list = {} 
-inputted_list = "bananas tomatoes apples bread"
-
-
-def add_items(list, inputted_list) 
-    
+def add_items(inputted_list) 
+   	list = {}
     list_array = inputted_list.split(' ')
     
     list_array.each do |item|
         list[item] = 1
     end
-    
+    list
 end
 
-def new_item(list, item, quantity = 1)
-	list[item] = quantity 
-	p list
+def new_item(list, item, quantity=1)
+ 	if list[item] == nil
+		list[item] = quantity 
+	end
+	list
 end
 
-add_items(list, inputted_list)
-new_item(list, "milk")
+inputted_list = "bananas tomatoes apples bread"
+
+list = add_items(inputted_list)
+new_item(list, "bananas", 5)
+p list
 
