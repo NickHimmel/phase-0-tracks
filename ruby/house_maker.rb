bd21e5d762ca7ccad4c9d1e9f12ca16d48b170c8
NickHimmel <himmel.nick@gmail.com>
2016-11-09 16:47:50 +0100

    finish house_maker.rb

--- ruby/house_maker.rb
+++ ruby/house_maker.rb
@@ -29,8 +29,8 @@ house = {}
 def print_house(house)
 	puts "------------------"
 	puts "Current house configuration:"
-	house.each do |room_name, items|
-		puts "#{room_name}: #{items}"
+	house.keys.each_with_index do |room_name, index|
+		puts "#{index} - #{room_name}: #{house[room_name]}"
 	end
 	puts "------------------"
 end
@@ -47,11 +47,29 @@ while can_add_rooms
 	break if room_name == 'done'
 	# Otherwise, add the room to the house
 	can_add_rooms = add_room_to_house!(house, room_name)
+	if !can_add_rooms
+		puts "sorry, that's too many rooms!"
+	end
 	print_house(house)
 end
 
-
-
+# Let the user add items to rooms
+# In an infinite loop:
+	# Ask the user for the number of the room they want to add
+	# items to
+	# If the user is done, break
+	# Otherwise, add the item to the room
+	#print the new house configuration
+loop do
+	puts "Enter the number of the room to add an item to (or type 'done'):"
+	inputted_idx = gets.chomp
+	break if inputted_idx == "done"
+	room_idx = inputted_idx.to_i
+	puts "Enter the item to add: "
+	item_to_add = gets.chomp
+	add_item_to_room!(house, house.keys[room_idx], item_to_add)
+	print_house(house)
+end
 # TEST CODE
 
 # rooms = ['Living room', 'bedroom', 'bathroom', 'kitchen', 'bedroom 2', 'bedroom 3']
