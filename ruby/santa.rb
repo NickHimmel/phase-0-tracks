7191066ec045713f8926c769360adef719ba4847
NickHimmel <himmel.nick@gmail.com>
2016-11-09 12:12:53 +0100

    add three attribute-changing methods, add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,8 +26,61 @@ class Santa
 		end
 	end 
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking << reindeer
+		puts "Bad #{reindeer}, you know what you did! To the back of the list with you!"
+		puts "My new list is: #{@reindeer_ranking}"
+	end
+	#getter methods
+	def age 
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def gender
+		@gender
+	end 
+
+	#setter methods
+	def gender=(new_gender)
+		@gender = new_gender
+	end
 end
 
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
+
+Kris_Kringle = Santa.new("male", "white")
+Kris_Kringle.get_mad_at("Vixen")
+puts "Kris Kringle is #{Kris_Kringle.age} years old and a #{Kris_Kringle.ethnicity} #{Kris_Kringle.gender}"
+Kris_Kringle.gender = "female"
+# Kris_Kringle.celebrate_birthday
+27.times do 
+	Kris_Kringle.celebrate_birthday
+end
+puts "Kris Kringle is #{Kris_Kringle.age} years old and a #{Kris_Kringle.ethnicity} #{Kris_Kringle.gender}"
 # Kris_Kringle = Santa.new
 # St_Nick = Santa.new
 # Father_Christmas = Santa.new
