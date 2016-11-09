234778cbd0833d74069300500b87eec302d6b047
NickHimmel <himmel.nick@gmail.com>
2016-11-09 17:27:32 +0100

    add module mixin and two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,36 @@
+# module Shout
+# 	 # we'll put some methods here soon, but this code is fine for now!
+# 	 def self.yell_angrily(words)
+# 	 	words + "!!!" + " :("
+# 	 end
+
+# 	 def self.yell_happily(words)
+# 	 	words + "!!!" + " :)"
+# 	 end
+# end
+
+# p Shout.yell_angrily("hello")
+# p Shout.yell_happily("hello")
+
 module Shout
-	 # we'll put some methods here soon, but this code is fine for now!
-	 def self.yell_angrily(words)
-	 	words + "!!!" + " :("
+
+	 def yell_angrily(words)
+	 	words + "!!!" + " :-("
 	 end
 
-	 def self.yell_happily(words)
-	 	words + "!!!" + " :)"
+	 def yell_happily(words)
+	 	words + "!!!" + " :-)"
 	 end
+
 end
 
-p Shout.yell_angrily("hello")
-p Shout.yell_happily("hello")
+class Grumpy_old_man
+	include Shout
+end
+
+class Toddler 
+	include Shout
+end
+
+Grampa = Grumpy_old_man.new
+Grampa.yell_angrily("I am mad as hell and I am not going to take it anymore")
