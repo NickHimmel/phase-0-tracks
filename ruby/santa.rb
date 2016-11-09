eefac9f09a85e48934cd4d38523a07dacd2139bb
NickHimmel <himmel.nick@gmail.com>
2016-11-09 10:08:18 +0100

    add instances of santa, test methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,32 @@
 class Santa
 
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
+	end
+
+	def naughty_nice(list_check)
+		puts "I am making a list and checking it twice. Gonna find out who's naughty or nice..."
+		if list_check == "nice"
+			puts "You have been good for goodness sake!"
+		else 
+			puts "Naughty! You get a single lump of coal!"
+		end
+	end 
+
+	def initialize
+		puts "Initialize Santa instance"
+	end
+
 end
+
+Kris_Kringle = Santa.new
+St_Nick = Santa.new
+Father_Christmas = Santa.new
+
+St_Nick.eat_milk_and_cookies("chocolate chip")
+Kris_Kringle.naughty_nice('naughty')
+Father_Christmas.speak
