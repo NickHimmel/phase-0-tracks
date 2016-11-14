89ee2ef95d4be971e7dbc5c9ddcccaa7daf0451d
NickHimmel <himmel.nick@gmail.com>
2016-11-14 12:06:25 +0100

    add full_dance_card method and call it from queue_dance_with method. Pass final test, in the green

--- ruby/week_6_practice_assessment/ballet.rb
+++ ruby/week_6_practice_assessment/ballet.rb
@@ -23,6 +23,7 @@ class Dancer
 	# call full_dance_card here to check if dance card if full
 	def queue_dance_with(dancer)
 		@card << dancer
+		full_dance_card
 		dancer
 	end
 
@@ -32,7 +33,12 @@ class Dancer
 		@card.delete_at(0)
 		next_dance
 	end
-# create a method full_dance_card that caps your dance card at 5, prints a message that your dance card is filled and deletes the last dancer entered. 
-	# def full_dance_card
-	# end 
+# 	create a method full_dance_card that caps your dance card at 5, prints a message that your dance card is filled and deletes the last dancer entered. 
+	def full_dance_card
+		if @card.length > 5
+			puts "Your dance card is full! You have to dance with #{@card[0]} before you can add #{@card[-1]}"
+			@card.delete_at(-1)
+		end
+		@card
+	end 
 end
