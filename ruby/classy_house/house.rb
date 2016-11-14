291bd471d0737506d61b4b36d12f2c19ce34e757
NickHimmel <himmel.nick@gmail.com>
2016-11-14 16:41:03 +0100

    update all

---
+++ ruby/classy_house/house.rb
@@ -0,0 +1,12 @@
+# # Build a house out of classes
+
+# HOUSE CLASS
+# Attributes:
+# - rooms (a collection of Room instances)
+# Methods:
+# -getter for rooms
+# -add room (only allows up to 10 rooms)
+# -square_footage (adds up the house's square footage and returns it as an integer)
+# -total_value (adds up value of it all rooms)
+# -to_s override
+
