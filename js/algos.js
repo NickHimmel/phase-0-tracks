a7b3e0a4df6561738c0d46622ee94b7b8a6bf50f
NickHimmel <himmel.nick@gmail.com>
2016-11-15 16:22:35 +0100

    add function longest_word

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,14 @@
-write a function that takes an array of words and returns the longest word 
-for example ["long phrase","longest phrase","longer phrase"] should return "longest phrase"
+// write a function that takes an array of words and returns the longest word 
+// for example ["long phrase","longest phrase","longer phrase"] should return "longest phrase"
 
-- create a function called longest_word and pass in an array
-- 	create a variable called longest_str
--	call a for loop on array to iterate through the array with variable i
--		call another for loop within the first loop with variable x
--			in the second loop call an if loop, if your array at index i is longer then your array at index x
-				then longest_str is equal array at index i
--	return longest_str
+// - create a function called longest_word and pass in an array
+function longest_word(arry)
+// - 	create a variable called longest_str
+		var longest_str
+// -	call a for loop on array to iterate through the array with variable i
+// -		call another for loop within the first loop with variable x
+// -			in the second loop call an if loop, if your array at index i is longer then your array at index x
+// 				then longest_str is equal array at index i
+// -	return longest_str
+		return longest_str
+end
