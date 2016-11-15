ce06f1c1c529405713bab5ffd471420fe5910274
NickHimmel <himmel.nick@gmail.com>
2016-11-15 15:31:50 +0100

    add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Create a function called reverse with a parameter that should reverse a string
+// 	pass in the word you want to reverse call it str_forward
+// 	declare a new variable called str_backward to hold reversed string
+// 	loop backward over str_forward with a for loop, var i should equal the length of str_forward
+// 		add each letter of str_forward to str_backward as you loop using i as the index and using + to add it to the string
+// 	return the new variable
+// end function
+
+// console log the function reverse passing in the string "hello world"
