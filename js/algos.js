456122f996ffd47b9e682cd58663c4aa8ad48ccc
NickHimmel <himmel.nick@gmail.com>
2016-11-15 16:16:58 +0100

    create algos.js add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+write a function that takes an array of words and returns the longest word 
+for example ["long phrase","longest phrase","longer phrase"] should return "longest phrase"
+
+- create a function called longest_word and pass in an array
+- 	create a variable called longest_str
+-	call a for loop on array to iterate through the array
+-	return longest_str
