468569b7b6d2c7f1b02b61f54e323c4f1b679899
NickHimmel <himmel.nick@gmail.com>
2016-11-17 18:30:48 +0100

    finish pairing

--- js/data_scructures.js
+++ js/data_scructures.js
@@ -16,3 +16,28 @@ for(var i = 0; i < colors.length; i++) {
 }
 
 console.log(horse_color);
+
+function Car(color, make, engine, wheels) {
+
+	console.log("It is a brand new car:", this);
+	this.color = color;
+	this.make = make;
+	this.engine = engine;
+	this.wheels = wheels;
+
+	this.has_engine = function() { 
+		if (engine == true){
+			console.log("whroom, whroom");
+		}
+		else {
+			console.log("sputter");
+		}
+	};
+
+}
+
+var newCar = new Car('grey', 'Mercedes', true, 4);
+var anotherCar = new Car('orange', 'Volvo', true, 4);
+newCar.has_engine();
+anotherCar.has_engine();
+
