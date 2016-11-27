4349bcae26caa7bb2478d9adb712a5b9b527849f
NickHimmel <himmel.nick@gmail.com>
2016-11-27 16:39:57 -0500

    add print feature

--- databases/lunch/lunch_decider.rb
+++ databases/lunch/lunch_decider.rb
@@ -38,7 +38,7 @@ Create a while true loop
 require 'sqlite3'
 
 db = SQLite3::Database.new("lunch.db")
-# db.results_as_hash = true
+db.results_as_hash = true
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS lunch(
@@ -58,19 +58,36 @@ def add_lunch(db, dish, restaurant, details, price, have_tried, desire)
   db.execute("INSERT INTO lunch (dish, restaurant, details, price, have_tried, desire) VALUES (?, ?, ?, ?, ?, ?)", [dish, restaurant,details, price, have_tried, desire])
 end
 
+lunch_options = db.execute("SELECT * FROM lunch")
 #USER CODE
 
 puts "\n*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*"
 puts "\nThe Lunch Decider"
+puts "A handy list of all the dishes you like and crave!"
 puts "\n-------------------------------------------------------------"
 
 i = true
 
 while i == true 
-	puts "Type 'enter' to add an exciting lunch option! \nType 'exit' to exit"
+	puts "Type 'enter' to add an exciting lunch option! \nType 'print' to view your lunch options \nType 'exit' to exit"
 	user_input = gets.chomp.downcase
+
 	if user_input == "enter"
-		puts user_input
+
+		puts "What is the name of the dish?"
+		dish = gets.chomp
+		puts "What is the name of the restaurant?"
+		restaurant = gets.chomp
+		puts "What is in the dish?"
+		details = gets.chomp
+		puts "How much does it cost? (no $ sign no decimal, 1.25 is 125)"
+		price = gets.chomp.to_i
+		puts "Have you tried it before (true or false)"
+		have_tried = gets.chomp
+		puts "How much are you craving it (1 to 10, 10 being the most)"
+		desire = gets.chomp.to_i
+	elsif user_input == "print"
+		p lunch_options
 	elsif user_input == "exit"
 		i = false
 	end
