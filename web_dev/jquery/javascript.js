c8e204b06f885920778468b288a3eb1b25a452d0
NickHimmel <himmel.nick@gmail.com>
2016-12-02 18:40:24 -0500

    add cards

--- web_dev/jquery/javascript.js
+++ web_dev/jquery/javascript.js
@@ -4,22 +4,40 @@ var game_cards = {
 	cards: [
 		{
 			id: 1,
-			img: "back_of_card_"
+			img: "back_of_card_1.png"
+		},{
+			id: 1,
+			img: "back_of_card_1.png"
 		},{
 			id: 2,
-			img: "back_of_card_"
+			img: "back_of_card_2.png"
+		},{
+			id: 2,
+			img: "back_of_card_2.png"
 		},{
 			id: 3,
-			img: "back_of_card_"
+			img: "back_of_card_3.png"
+		},{
+			id: 3,
+			img: "back_of_card_3.png"
 		},{
 			id: 4,
-			img: "back_of_card_"
+			img: "back_of_card_4.png"
+		},{
+			id: 4,
+			img: "back_of_card_4.png"
 		},{
 			id: 5,
-			img: "back_of_card_"
+			img: "back_of_card_5.png"
+		},{
+			id: 5,
+			img: "back_of_card_5.png"
 		},{
 			id: 6,
-			img: "back_of_card_"
+			img: "back_of_card_6.png"
+		},{
+			id: 6,
+			img: "back_of_card_6.png"
 		}
 	]
 }
@@ -28,9 +46,10 @@ var game_cards = {
 
 var controller = {
 
-	card_link: function () {
-		var img_link = "<img src=\"" + game_cards.cards[0].img +  "\" alt=\"Memory Card\">"
-		return img_link
+	card_link: function (i) {
+		 
+		var img_link = "<img src=\"img/" + game_cards.cards[i-1].img + "\" alt=\"memory card\">";
+		return img_link;
 	}
 
 }
@@ -38,9 +57,14 @@ var controller = {
 var view = {
 
 	display_cards: function () {
-		for (var i = Things.length - 1; i >= 0; i++) {
-			Things[i]
+		for (var i = 1; i <= 12; i++) {
+			var card_spot = "#card_" + i;
+			console.log(card_spot)
+			// console.log(controller.card_link(i));
+			$(card_spot).append(controller.card_link(i));
 		}
 	}
 
 }
+
+console.log(view.display_cards())
