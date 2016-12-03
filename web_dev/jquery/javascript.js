532b8500d71b888b2b983052652481103b3f4ca7
NickHimmel <himmel.nick@gmail.com>
2016-12-02 19:48:22 -0500

    add shuffle function

--- web_dev/jquery/javascript.js
+++ web_dev/jquery/javascript.js
@@ -63,8 +63,37 @@ var view = {
 			// console.log(controller.card_link(i));
 			$(card_spot).append(controller.card_link(i));
 		}
+	},
+
+	//  Fisher-Yates (aka Knuth) Shuffle algorithm
+	//  https://github.com/coolaj86/knuth-shuffle
+
+	shuffle: function(array){
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  // While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    	randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+	    // And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+	    	array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+	randomize: function(array){
+		$("#randomize")
 	}
 
 }
 
 console.log(view.display_cards())
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+arr = view.shuffle(arr);
+console.log(arr);
